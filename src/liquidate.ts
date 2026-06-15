36302cf7aecfecfc88bb3e8eb82585d94c1bd989
Daniel Tehrani <contact@dantehrani.com>
2026-06-16 00:30:31 +0900

    u

--- src/liquidate.ts
+++ src/liquidate.ts
@@ -17,80 +17,100 @@ const getPositionKey = ({
 
 const getPositionsToSkip = new Set<string>();
 
+const liquidatePosition = async ({
+  marketId,
+  borrower,
+}: {
+  marketId: Hex;
+  borrower: Hex;
+}) => {
+  const positionKey = getPositionKey({
+    marketId,
+    borrower,
+  });
+
+  if (getPositionsToSkip.has(positionKey)) {
+    return;
+  }
+
+  const getMarketParamsTimer = st('getMarketParams');
+  const market = await getMarketParams({
+    marketId,
+  });
+  ed(getMarketParamsTimer);
+
+  const getPositionTimer = st('getPosition');
+  const position = await getPosition({
+    marketId,
+    borrower,
+  });
+  ed(getPositionTimer);
+
+  if (position.borrowShares === 0n) {
+    getPositionsToSkip.add(positionKey);
+    return;
+  }
+
+  try {
+    const liquidateResult = await liquidate({
+      marketParams: market,
+      borrower,
+      seizedAssets: 0n,
+      repaidShares: position.borrowShares,
+      data: '0x',
+    });
+
+    logger.info('Liquidated position', {
+      marketId,
+      borrower,
+      liquidateResult,
+    });
+  } catch (error) {
+    let revertReason: string | undefined;
+
+    if (error instanceof BaseError) {
+      const revertError = error.walk(
+        e => e instanceof ContractFunctionRevertedError
+      );
+
+      if (revertError instanceof ContractFunctionRevertedError) {
+        revertReason = revertError.reason ?? revertError.data?.errorName;
+      }
+    }
+
+    if (!revertReason) {
+      logger.error('Unknown error:', error);
+      return;
+    }
+
+    if (revertReason === 'position is healthy') {
+      logger.info('Position is healthy');
+      return;
+    }
+
+    if (revertReason === 'inconsistent input') {
+      logger.error('Inconsistent input', {
+        marketId,
+        borrower,
+        revertReason,
+      });
+      // Both seizedAssets and repaidShares set (or both zero)
+      getPositionsToSkip.add(positionKey);
+    }
+  }
+};
+
 const main = async () => {
   for (const borrower of borrowers) {
-    const positionKey = getPositionKey({
-      marketId: borrower.marketId as Hex,
-      borrower: borrower.borrower as Hex,
-    });
-
-    if (getPositionsToSkip.has(positionKey)) {
-      continue;
-    }
-
-    const getMarketParamsTimer = st('getMarketParams');
-    const market = await getMarketParams({
-      marketId: borrower.marketId as Hex,
-    });
-    ed(getMarketParamsTimer);
-
-    const getPositionTimer = st('getPosition');
-    const position = await getPosition({
-      marketId: borrower.marketId as Hex,
-      borrower: borrower.borrower as Hex,
-    });
-    ed(getPositionTimer);
-
-    if (position.borrowShares === 0n) {
-      getPositionsToSkip.add(positionKey);
-      continue;
-    }
-
     try {
-      const liquidateResult = await liquidate({
-        marketParams: market,
+      await liquidatePosition({
+        marketId: borrower.marketId as Hex,
         borrower: borrower.borrower as Hex,
-        seizedAssets: 0n,
-        repaidShares: position.borrowShares,
-        data: '0x',
-      });
-
-      logger.info('Liquidated position', {
-        marketId: borrower.marketId,
-        borrower: borrower.borrower,
-        liquidateResult,
       });
     } catch (error) {
-      let revertReason: string | undefined;
-
-      if (error instanceof BaseError) {
-        const revertError = error.walk(
-          e => e instanceof ContractFunctionRevertedError
-        );
-
-        if (revertError instanceof ContractFunctionRevertedError) {
-          revertReason = revertError.reason ?? revertError.data?.errorName;
-        }
-      }
-
-      if (!revertReason) {
-        logger.error('Unknown error:', error);
-        continue;
-      }
-
-      if (revertReason === 'position is healthy') {
-        logger.info('Position is healthy');
-      }
-
-      if (revertReason === 'inconsistent input') {
-        logger.error('Inconsistent input', {
-          marketId: borrower.marketId,
-          borrower: borrower.borrower,
-          revertReason,
-        });
-        // Both seizedAssets and repaidShares set (or both zero)
-        getPositionsToSkip.add(positionKey);
-      }
+      logger.error('Error liquidating position', {
+        error,
+      });
     }
 
     await sleep(1000);
